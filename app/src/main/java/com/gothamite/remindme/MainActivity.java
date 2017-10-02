64a30c7e66b3fe9a9fb867c21d5e4a33ec227d4b
Gothamite <kaercerbin@bk.ru>
2017-10-02 17:17:36 +0500

    Added Toolbar and Search Button.

--- app/src/main/java/com/gothamite/remindme/MainActivity.java
+++ app/src/main/java/com/gothamite/remindme/MainActivity.java
@@ -3,11 +3,30 @@ package com.gothamite.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolBar.inflateMenu(R.menu.menu);
     }
 }
